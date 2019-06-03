e530377865ee1b1e5b530408bba02b5a26f5f5eb
Michal Soucek <inasuk@gmail.com>
2019-06-03 07:52:01 +0200

    I'm probably lost right now.

--- lib/tagwork.php
+++ lib/tagwork.php
@@ -102,9 +102,6 @@ class Tag {
 	}
 
 	public function getBaseName(): string {
-		if ($this->hidden) 
-			return "!" . $this->tag;
-
 		return $this->tag;
 	}
 
@@ -130,7 +127,7 @@ class Tag {
 
 	public static function parseBaseName(string $tag) {
 		$tg = explode(":", $tag);
-		return $tg[0];
+		return $tg[0][0] == "1" ? substr($tg[0], 1) : $tg[0];
 	}
 
 	public static function parseSelectedName(string $tag) {
